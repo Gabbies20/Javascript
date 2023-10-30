382b67c069c3fae64a55f80d5edca1a529b3399d
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-30 12:46:38 +0100

    Eje2 hecho

--- FUNCIONES/eje3.js
+++ FUNCIONES/eje3.js
@@ -2,10 +2,9 @@
  * Realizar un programa que calcule el número de cifras de un número. Deberá implementarse una función numCifras(numero) que devuelva el número de cifras del mismo. Utiliza el operador spread.
  */
 
-function numCifras(numero){
-
-    numero.toString();
-    for( let i = 0; i < numero.lenght; i++){
-        console.log(i);
-    }
+function numCifras(numero) {
+    var cifras = numero.toString();
+    console.log(cifras.length);
 }
+
+numCifras(4569)
