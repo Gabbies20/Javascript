e84504600ae5b15504a4b4b7e4a6cb79d96d584d
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-10-30 14:45:43 +0100

    Ejercicio 9d en proceso

---
+++ ARRAYS/9d.js
@@ -0,0 +1,17 @@
+/**
+ * Escribe una función llamada sumaArgPares que sume todos los argumentos pares que se pasen a la función.
+ */
+
+function sumaArgPares(arr){
+    var suma = 0;
+
+    for (let i = 0; i < arguments.length; i++) {
+      if (i % 2 === 0) { 
+        suma += arguments[i];
+      }
+    }
+  
+    return suma;
+}
+
+console.log(sumaArgPares(1, 2, 3, 4, 5, 6));
--- ARRAYS/eje10.js
+++ ARRAYS/eje10.js
@@ -11,7 +11,7 @@
 
  */
 
-let tripleAndFilter = (arr) => arr.map((value)=> value * 3).filter((value)=> value % 5 === 0);
+let tripleAndFilter = (arr) => arr.map((value) => value * 3).filter((value) => value % 5 === 0);
 
 console.log(tripleAndFilter([3, 6, 9, 12, 15, 18, 21, 24]));
 //En el arreglo [3, 6, 9, 12, 15, 18, 21, 24], después de triplicar los valores, obtenemos [9, 18, 27, 36, 45, 54, 63, 72]. El único valor divisible por 5 en este arreglo es 45, por lo que se muestra en la consola.
@@ -29,9 +29,9 @@ console.log(tripleAndFilter([3, 6, 9, 12, 15, 18, 21, 24]));
 }
  */
 
-let doubleOddNumbers = (a) => a.filter((val)=>val % 2 !== 0).map((val)=> val* 2);
+let doubleOddNumbers = (a) => a.filter((val) => val % 2 !== 0).map((val) => val * 2);
 
-console.log(doubleOddNumbers([7,5,9,86,15]));
+console.log(doubleOddNumbers([7, 5, 9, 86, 15]));
 
 //**************************************************** */
 
@@ -40,9 +40,9 @@ console.log(doubleOddNumbers([7,5,9,86,15]));
  * Asegurate de que la funcion se llame bar:
  */
 
-function bar(){
+function bar() {
   let txt = '';
-  for(let i in arguments){
+  for (let i in arguments) {
     txt += arguments[i];
   }
   return txt;
@@ -53,3 +53,21 @@ const resultado = bar('Hola', ' ', 'mundo', '!');
 const longitud = resultado.length;
 
 console.log(longitud); // Esto mostrará la longitud de la cadena en la consola
+
+let bar = () => {
+  let text = '';
+  for(let i in arguments) {
+    text+=arguments[i];
+}
+}
+
+/**
+ * const bar = (...args) => args.reduce((txt, arg) => txt + arg, '');
+
+console.log(bar('Hola', ' ', 'mundo', '!'));
+
+ */
+
+/**
+ * En esta versión, hemos utilizado una función flecha y el operador rest (...args) para recoger todos los argumentos en un arreglo llamado args. Luego, hemos utilizado el método reduce() para concatenar los elementos del arreglo en una cadena, y hemos inicializado txt como una cadena vacía ('') en el proceso de reducción.
+ */
