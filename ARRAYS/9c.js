257b329da6f965995a66965f3b67b600dbbc872e
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-01 23:59:13 +0100

    9c de Arrays hecho

--- ARRAYS/9c.js
+++ ARRAYS/9c.js
@@ -2,5 +2,10 @@
  Escribe una función llamada uneArrays que acepte un número variable de parámetros (cada uno de ellos será un array) y devuelva un nuevo array con todos los parámetros concatenados
  */
 
- 
+ function uneArrays(...arrays){
+    var nuevo = [];
+    
+    return nuevo.concat(...arrays);
+ }
 
+ console.log(uneArrays([1,2,3],[4,5,6]))
