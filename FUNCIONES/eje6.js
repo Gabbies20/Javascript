84472fcdc6c1ea317330b4b2b2ac0b0eca8db361
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-01 14:20:41 +0100

    Hecho ejercicio 6 de funciones

--- FUNCIONES/eje6.js
+++ FUNCIONES/eje6.js
@@ -1,4 +1,16 @@
 /**
  * Hacer un programa que sume todos los parámetros pasados como argumentos de entrada en la llamada. El número de argumentos de entrada es desconocido.
  */
+function sumaParametros(...params) {
+    
+    var suma = 0;
 
+    for (let index = 0; index < params.length; index++) {
+        const parametro = params[index];
+
+        suma += parametro;
+    }
+    return suma;
+}
+
+console.log(sumaParametros(1, 8, 78));
