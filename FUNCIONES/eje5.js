4123a4830dc9b61e415b92c28ccbb24a74f68485
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-01 21:29:41 +0100

    Ejercicio 5 y 8 de Funciones hechos

--- FUNCIONES/eje5.js
+++ FUNCIONES/eje5.js
@@ -1,3 +1,43 @@
 /**
  * Hacer un programa para generar el siguiente primo a uno dado. Deben usarse funciones. 
  */
+
+
+function divisores(numero){
+
+    var d = [];
+
+    for ( let i =1; i <= numero; i++){
+        if(numero % i == 0){
+            d.push(i);
+        }
+    }
+
+    return d;
+}
+
+console.log(divisores(10));
+
+
+function esPrimo(numero){
+    if(divisores(numero).length == 2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log((esPrimo(10)));
+
+
+function siguientePrimo(numero){
+
+    var i = numero +1;
+
+    while(esPrimo(i)== false){
+        i++;
+    }
+
+    return i;
+}
+console.log((siguientePrimo(13)))
--- FUNCIONES/eje8.js
+++ FUNCIONES/eje8.js
@@ -1,3 +1,18 @@
 /*
 Realizar la función esPalindromo(cadena) de manera recursiva. 
 */
+function esPalidroma(palabra){
+    if(palabra.length <= 1){
+        return true;
+    }else if(palabra[0]==palabra[palabra.length-1]){
+        return esPalidroma(palabra.slice(1,palabra.length-1));
+    }else{
+        return false;
+    }
+}
+
+console.log(esPalidroma('aeiouoiea'));
+
+
+
+
