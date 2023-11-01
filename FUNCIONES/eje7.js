9c3043c8f04b627035a615a80ded6f032fd12650
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-01 14:37:39 +0100

    Hecho ejercicio 7 de funciones

--- FUNCIONES/eje7.js
+++ FUNCIONES/eje7.js
@@ -1,3 +1,12 @@
 /**
  * Escribe una función que reciba como argumento de entrada un array de números y averigue utilizando métodos del objeto Math cuál es el menor y el mayor de ellos.
  */
+
+function menorMayor(array){
+    var mayor = Math.max(...array);
+    var menor = Math.min(...array);
+
+    return `EL mayor de los elementos del array es ${mayor} y el menor es ${menor}`
+}
+
+console.log(menorMayor([5,8,2,1,89,45,10]))
