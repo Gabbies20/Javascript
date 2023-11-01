2cd4501b98fdc71e51cc529d4e2539043775bebf
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-01 13:55:39 +0100

    Hecho ejercicio 4 de funciones

--- FUNCIONES/eje4.js
+++ FUNCIONES/eje4.js
@@ -1,3 +1,13 @@
 /**
  * Realizar una función que pase una cantidad de Mbyte, Kbytes y bytes a bytes. Probadla en una página.
  */
+
+function convertirBytes(cantidadMbytes, cantidadKbytes, cantidadBytes) {
+    
+    const totalBytes = (cantidadMbytes * 1048576) + (cantidadKbytes * 1024) + cantidadBytes;
+    
+    return totalBytes;
+  }
+
+  const resultado = convertirBytes(2, 500, 70);
+  console.log(`La cantidad total en bytes es: ${resultado}`);
