3b7a12affb55fee151b1ee9ae6299ad4849e8bf6
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-05 15:29:14 +0100

    Ejercico 21, planteamiento

--- ARRAYS/19bis.js
+++ ARRAYS/19bis.js
@@ -1,8 +1,6 @@
 /**
  * Hacer un programa en el que el usuario que introduzca, nombre, apellidos, DNI y fecha de nacimiento separado por comas. Esta entrada de datos se repetirá hasta que el usuario introduzca la cadena vacía. El programa debe guardar los datos en un array bidimensional.
  */
-
-
 function obtenerDatos() {
 
     var array = [];
