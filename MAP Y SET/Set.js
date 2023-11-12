97e90242fc9add3347c07602ba2d20d6840a8af9
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-11-12 23:35:53 +0100

    Set

---
+++ MAP Y SET/Set.js
@@ -0,0 +1,9 @@
+/**
+ * SET:Permiten almacenar datos, sin admitir valores duplicados, eliminando valores duplicados.
+ */
+
+const lista = new Set();
+lista.add(8);
+lista.add(6);
+
+console.log(lista);
