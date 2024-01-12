bdc0d0513bef095eb1dbb1876f5dd7c7cd897565
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2024-01-12 01:46:24 +0100

    Eventos ejercicios

--- Boletin 5/EVENTOS/eje13.js
+++ Boletin 5/EVENTOS/eje13.js
@@ -1,5 +1,20 @@
-window.addEventListener('load',()=>{
-    document.getElementById('miFormulario').addEventListener('click',()=>{
-
+window.addEventListener('load', () => {
+    document.getElementById('enviar').addEventListener('click', () => {
+        convertirAMinusculas();
     });
+
+    function convertirAMinusculas() {
+        const inputTexto = document.getElementById('miTexto');
+        const resultadoElemento = document.getElementById('resultado');
+
+        if (inputTexto.value.trim() !== '') {
+            //inputTexto.value = inputTexto.value.toLowerCase();
+            const resultado = inputTexto.value.toLowerCase();
+            resultadoElemento.innerText = 'Texto convertido: ' + resultado;
+
+        } else {
+            alert('El texto está vacío.');
+            resultadoElemento.innerText = ''; // Limpiar el resultado si el texto está vacío
+        }
+    }
 });
