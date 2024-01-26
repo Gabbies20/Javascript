381ed4d9f007bbc45220000bf186d649dd4d118a
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2024-01-26 03:04:00 +0100

    Repaso terminado

--- EXAMEN TEMA5 Y TEMA6/8.js
+++ EXAMEN TEMA5 Y TEMA6/8.js
@@ -2,16 +2,16 @@
  * 
  */
 
-window.addEventListener('load',iniciar);
+window.addEventListener('load', iniciar);
 
-function iniciar(){
+function iniciar() {
 
     var div = document.getElementById('todos-los-viajes');
 
     var datos = [
-        {src:"mostrar.jpeg", desc: "descripcion1", specs: ["spec11", "spec12"]},
-        {src:"ocultar.jpeg", desc: "descripcion2", specs: ["spec21", "spec22"]},
-        ];
+        { src: "mostrar.jpeg", desc: "descripcion1", specs: ["spec11", "spec12"] },
+        { src: "ocultar.jpeg", desc: "descripcion2", specs: ["spec21", "spec22"] },
+    ];
 
     //Crear el elelmento h1 para el subtitulo:
     var h1 = document.createElement('h1');
@@ -25,29 +25,88 @@ function iniciar(){
     div.appendChild(ul);
 
 
-    for(let i = 0; i < datos.length; i++){
+    for (let i = 0; i < datos.length; i++) {
         console.log(i);
 
         var li = document.createElement('li');
+        ul.appendChild(li);
         var img = document.createElement('img');
-        img.setAttribute('src',datos[i].src);
+        img.setAttribute('src', datos[i].src);
         img.width = 50;
         li.appendChild(img);
+
+        //creo elemento p para descripcion
         var p = document.createElement('p');
         var txtP = document.createTextNode(datos[i].desc);
         p.appendChild(txtP);
-        ul.appendChild(p);
-        ul.appendChild(li);
+        li.appendChild(p);
+
 
         var ul2 = document.createElement('ul');
-        for( x in i.specs){
-
-            let li = document.createElement('li');
-            let txtLi = document.createTextNode(datos[i].specs)
+        li.appendChild(ul2);
+        for (x of datos[i].specs) {
+            var li1 = document.createElement('li');
+            ul2.className = 'specs'
+            ul2.appendChild(li1)
+            var txtLi = document.createTextNode(x);
+            li1.appendChild(txtLi);
         }
 
+        /**
+         * for(let j = 0; j < datos[i].specs.length; j++){
+            var li1 = document.createElement('li');
+            li1.className = 'specs';
+            ul2.appendChild(li1);
+            var txtLi = document.createTextNode(datos[i].specs[j]);
+            li1.appendChild(txtLi);
+        }
+         */
 
+
+        img.addEventListener('click', ocultarInformacion);
+
+
+
+
+        function ocultarInformacion(e) {
+            var img = e.currentTarget;
+            var padre = img.parentNode
+            console.log(img);
+            console.log(padre);
+            console.trace()
+
+
+            //Obtener los hermanos de la etiquetan img:
+            //padre.childNodes devuelve una colección de todos los nodos secundarios (hijos) del elemento padre (<li> en este caso). 
+            // Obtener todos los nodos secundarios del elemento padre
+            var padreChildNodes = padre.childNodes;
+            console.log("padreChildNodes:");
+            for (var i = 0; i < padreChildNodes.length; i++) {
+                console.log(padreChildNodes[i]);
+            }
+
+
+
+            var hijosChildren = [...padre.childNodes].filter((h) => {
+                r = h != img;
+                return r;
+            })
+
+
+            console.log(hijosChildren);
+
+
+            //Como hijosChildren es un array debo recorrerlos:
+            for(hijo of hijosChildren){
+                //e.style.display = 'none';
+                if(hijo.style.display != 'none'){
+                    hijo.style.display = 'none';
+                }else{
+                    hijo.style.display = 'block';
+                }
+            }
+        }
 
     }
-        
+
 }
